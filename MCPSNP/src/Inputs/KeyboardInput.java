70a9288d4f65d2e15c9ef2c50ff120739cf28be2
PANANG6425 <100993424+PANANG6425@users.noreply.github.com>
2024-03-21 13:25:50 +0700

    Update KeyboardInput.java
    Updateนี้เล็กน้อยเหมือนกัน

--- MCPSNP/src/Inputs/KeyboardInput.java
+++ MCPSNP/src/Inputs/KeyboardInput.java
@@ -2,7 +2,7 @@ package Inputs;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+import static utilz.Constants.Directions.*;
 import Main.GamePanel;
 
 
@@ -22,22 +22,30 @@ public class KeyboardInput implements KeyListener{
 	public void keyPressed(KeyEvent e) {
 		switch(e.getKeyCode()) {
 		case KeyEvent.VK_W:
-			gamePanel.changeYDelta(-5);
-			break;
-		case KeyEvent.VK_A:
-			gamePanel.changeXDelta(-5);
+			gamePanel.setDirection(Up);
 			break;
 		case KeyEvent.VK_S:
-			gamePanel.changeYDelta(5);
+			gamePanel.setDirection(Down);
+			break;
+		case KeyEvent.VK_A:
+			gamePanel.setDirection(Left);
 			break;
 		case KeyEvent.VK_D:
-			gamePanel.changeXDelta(5);
+			gamePanel.setDirection(Right);
 			break;
 	}
 }
 	@Override
 	public void keyReleased(KeyEvent e) {
-
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_W:
+		case KeyEvent.VK_S:
+		case KeyEvent.VK_A:
+		case KeyEvent.VK_D:
+			
+			break;
+			
+	}
 		}
 		
 	}
