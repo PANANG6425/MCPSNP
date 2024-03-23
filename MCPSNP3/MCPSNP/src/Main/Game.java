c96401496dd151d55e27baaadfc19cc5db24226b
JengCharat <51010691+JengCharat@users.noreply.github.com>
2024-03-23 17:36:07 +0700

    Add files via upload

--- MCPSNP3/MCPSNP/src/Main/Game.java
+++ MCPSNP3/MCPSNP/src/Main/Game.java
@@ -20,65 +20,62 @@ public class Game {
 
 			if(hittime >  5) {
 				System.out.println("ENDDDDDDDDDDDDDDDDDDDD");
-				gamePanel.loadBackground_dead();
-				gamePanel.setBgX(0);
-				gamePanel.setBgY(0);
-				gamePanel.updatePos();
-				gamePanel.repaint();
+				gamePanel.state = 5;
 				
 			}
-			while(gamePanel.moving) {
-				gamePanel.updatePos();
-				gamePanel.repaint();
-				System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-				System.out.println(gamePanel.getxDelta());
-				System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-				System.out.println(gamePanel.gh);
-				System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-				System.out.println("HIT TIME" + hittime);
-				//System.out.println(Math.abs(gamePanel.getxDelta() + gamePanel.ghostX) <1);
-				if(Math.abs(gamePanel.getxDelta() - (gamePanel.gh)) <=50){
-					gamePanel.setxDelta(100);
-					System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
-					gamePanel.setGhost_speed(400);
-					hittime +=1;
-				}
-				//1366,768
-				//20,430
-				System.out.println("x" + gamePanel.getBgX());
-				System.out.println("y" + gamePanel.getBgY());
-				
-				if(gamePanel.getxDelta() > 1366) {
-					gamePanel.setBgX(0);
-					gamePanel.setBgY(0);
-					gamePanel.stateadd();
-					gamePanel.loadBackground();
-					System.out.println("stop");
-					gamePanel.setDirection(Stop);
-					gamePanel.setxDelta(20);
-					gamePanel.setyDelta(498);
-				}
-				else if(gamePanel.getxDelta() < 0) {
-					gamePanel.setBgX(0);
-					gamePanel.setBgY(0);
-					gamePanel.stateneg();
-					gamePanel.loadBackground();
-					System.out.println("stop");
-					gamePanel.setDirection(Stop);
-					gamePanel.setxDelta(1300);
-					gamePanel.setyDelta(498);
-				}
-				
-				else {
-					try {
-						Thread.sleep(1); 
-					} catch (InterruptedException e) {
-						e.printStackTrace();
+			else {
+				while(gamePanel.moving) {
+					gamePanel.updatePos();
+					gamePanel.repaint();
+					System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+					System.out.println(gamePanel.getxDelta());
+					System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+					System.out.println(gamePanel.gh);
+					System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+					System.out.println("HIT TIME" + hittime);
+					//System.out.println(Math.abs(gamePanel.getxDelta() + gamePanel.ghostX) <1);
+					if(Math.abs(gamePanel.getxDelta() - (gamePanel.gh)) <=50){
+						gamePanel.setxDelta(100);
+						System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+						gamePanel.setGhost_speed(400);
+						hittime +=1;
+					}
+					//1366,768
+					//20,430
+					System.out.println("x" + gamePanel.getBgX());
+					System.out.println("y" + gamePanel.getBgY());
+					
+					if(gamePanel.getxDelta() > 1366) {
+						gamePanel.setBgX(0);
+						gamePanel.setBgY(0);
+						gamePanel.stateadd();
+						gamePanel.loadBackground();
+						System.out.println("stop");
+						gamePanel.setDirection(Stop);
+						gamePanel.setxDelta(20);
+						gamePanel.setyDelta(498);
+					}
+					else if(gamePanel.getxDelta() < 0) {
+						gamePanel.setBgX(0);
+						gamePanel.setBgY(0);
+						gamePanel.stateneg();
+						gamePanel.loadBackground();
+						System.out.println("stop");
+						gamePanel.setDirection(Stop);
+						gamePanel.setxDelta(1300);
+						gamePanel.setyDelta(498);
+					}
+					
+					else {
+						try {
+							Thread.sleep(1); 
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
 					}
 				}
 			}
+			System.out.println("breakkkkkkk");
 		}
-		System.out.println("breakkkkkkk");
 	}
-	
 }
